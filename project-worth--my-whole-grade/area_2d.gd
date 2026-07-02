a515f8c685e88533fae013c4811a6518077a2996
Rng2MAGS <rng2@students.mags.school.nz>
2026-07-03 02:57:10 +1200

    9
    bullet fix

---
+++ project-worth--my-whole-grade/area_2d.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var speed: float = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += transform.x * speed * delta
+	
+
+func _bullet_land(body: Node2D) -> void:
+	if body.is_in_group("asteroid"):
+		body.take_damage()
+		queue_free()
--- project-worth--my-whole-grade/scripts/asteroid.gd
+++ project-worth--my-whole-grade/scripts/asteroid.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 
+
+
+@export var player: CharacterBody2D
+@export var asteroid: CharacterBody2D
 const speed = 300.0
+var health: int = 100
+var damage: int = 20
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +16,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	pass
+
+func take_damage() -> void:
+	if health > 0:
+		health -= damage
+	elif health == 0:
+		queue_free()	
+	
+func _player_dmg(body: Node2D) -> void:
+	if body == player:
+		player.take_damage()
